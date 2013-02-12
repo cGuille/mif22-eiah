07458d56e71b2bd0e663699db54d5d26c4bd0131
Guillaume Charmetant <cguille.dev@gmail.com>
2013-02-12 10:30:07 +0100

    Change home page

--- src/pages.js
+++ src/pages.js
@@ -3,7 +3,7 @@
     var pages = window.pages = {};
 
     pages.home = function () {
-        return '<section class="identification"><form><p><label for="firstname">Entre ton prénom :</label><br/><input type="text" name="firstname" id="firstname"/></p><p><input type="radio" name="level" id="primaire" value="primaire" /><label for="primaire">Je suis en primaire</label><br/><input type="radio" name="level" id="college" value="college" /><label for="college">Je suis au collège</label></p><input type="submit" value="Commencer" class="btn"/></form></section>';
+        return '<div id="main"><header><div id="logo"><div id="logo_text"></div></div><nav><div id="menu_container"></div></nav></header><div id="site_content"><div class="content"><section class="identification"><form><p><label for="firstname">Entre ton prénom :</label><br/><input type="text" name="firstname" id="firstname"/></p><p><input type="radio" name="level" id="primaire" value="primaire" /><label for="primaire">Je suis en primaire</label><br/><input type="radio" name="level" id="college" value="college" /><label for="college">Je suis au collège</label></p><input type="submit" value="Commencer" class="btn"/></form></section></div></div><footer></footer></div><p>&nbsp;</p>';
     };
 
     pages.themes = function (profile) {
